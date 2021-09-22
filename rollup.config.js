1863ec99438cc8b71376e20cb4ebe174d153d1da
wangzheng <zheng.wang@fih-foxconn.com>
2021-09-22 14:04:32 +0800

    add babel react

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,34 @@
-import { terser } from 'rollup-plugin-terser';
+import { terser } from "rollup-plugin-terser";
+import babel from "rollup-plugin-babel";
+const globals = { react: "React" };
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: [
     {
-      file: 'dist/wing-use-auth.js',
-      name: 'wing-use-auth',
-      format: 'umd',
+      file: "dist/wing-use-auth.js",
+      name: "wing-use-auth",
+      format: "umd",
+      globals,
     },
     {
-      file: 'dist/wing-use-auth.es.js',
-      format: 'es',
+      file: "dist/wing-use-auth.es.js",
+      format: "es",
+      globals,
     },
     {
-      file: 'dist/wing-use-auth.amd.js',
-      format: 'amd',
+      file: "dist/wing-use-auth.amd.js",
+      format: "amd",
+      globals,
     },
     {
-      file: 'dist/wing-use-auth.cjs.js',
-      format: 'cjs',
+      file: "dist/wing-use-auth.cjs.js",
+      format: "cjs",
+      globals,
     },
   ],
-  plugins: [terser()],
+  external: ["react"],
+  plugins: [
+    babel({ exclude: "**/node_modules/**", runtimeHelpers: true }),
+    terser(),
+  ],
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("AUTH HOOK INITED");
+import React, { useContext, createContext, useState } from "react";
+const authContext = createContext();
+console.log(111);
+function useProviderAuth(initialState) {
+  const [authenticated, setAuthenticated] = useState(initialState);
+  const signin = (cb) => {
+    setAuthenticated(true);
+    cb && cb();
+  };
+
+  const signout = (cb) => {
+    setAuthenticated(false);
+    cb && cb();
+  };
+
+  return {
+    authenticated,
+    signin,
+    signout,
+  };
+}
+
+export function AuthProvider({ children, options }) {
+  const { initialState } = options;
+  const auth = useProviderAuth(initialState);
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+export function useAuth() {
+  return useContext(authContext);
+}
